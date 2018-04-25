6dc3efe2c727d5db2de1424de82b63bee9a18a54
Anorld Mukone <manorldsapiens@gmail.com>
2018-04-25 06:38:51 +0300

    Update: test_app.py
    1. Added a unittest for the remove_meal(id) method
    2. Going to add remove_meal(id) method to app.py
    3. If that works well, will use these 2 methods: signup and remove_meal() as a model for the remaining methods for just this stage.

--- api/v1/test_app.py
+++ api/v1/test_app.py
@@ -1,11 +1,14 @@
 import unittest
-from app import signup
+from app import signup, remove_meal
  
 class TddInFlaskAndelaTia(unittest.TestCase):
  
     def test_route_method_1(self):
         self.assertEqual(signup(), "New Account created")
         self.assertNotEqual(signup(), "")
+       
+        
+        self.assertIn("10", remove_meal(10))
         
  
  
