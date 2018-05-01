358c6c236ab9063c5b2ffbf6ad37c319dfb64b8e
Anorld Mukone <manorldsapiens@gmail.com>
2018-05-01 12:00:26 +0300

    Improved unittests

--- api/v2/test_app.py
+++ api/v2/test_app.py
@@ -6,37 +6,41 @@ import json
 class TestFlaskApi(unittest.TestCase):
     def setUp(self):
         self.app = app.app.test_client()
-        self.user = [{'name': 'Bob',
-                      'password'; 'xxy210'}] 
+        self.userr = {
+                        "name": "Bob",
+                        "email": "bob@gmail.com", 
+                        "password": "xxy210"
+                     }
 
 
-
+    
     def test_signup_new_user(self):
-        """Test API to create a new user"""
-        res = self.client().post('/auth/signup', data=self.user(0])
-        self.assertEqual(res.status_code, 201)
-        self.assertIn('Bob', str(res.data.name))
-
+        """"Test API to create a new user"""
+        response = self.app.post('/auth/signup', data= json.dumps(self.userr), content_type='application/json')
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 201)
+        self.assertIn('Bob', data['name'])
+    
 
     def test_all_meals(self):
         response = self.app.get('/meals/')
         data = json.loads(response.data)
         self.assertEqual(data['meals'], [
              {
-                 'id': 1,
-                 'name': 'Beef and Rice',
-                 'price': '25000',
-                 'time_created': 'Monday, 14th February 2018'
-             },
-             {
-                'id': 2,
-                 'name': 'Chicken and Matooke',
-                 'price': '35000',
-                 'time_created': 'Monday, 14th February 2018'
-             }
+                    'id': 1,
+                    'name': 'Beef and Rice',
+                    'price': '25000',
+                    'time_created': 'Monday, 14th February 2018'
+                },
+                {
+                    'id': 2,
+                    'name': 'Chicken and Matooke',
+                    'price': '35000',
+                    'time_created': 'Monday, 14th February 2018'
+                }
         ])
         self.assertEqual(response.status_code, 200)
-
+    
 
     def test_orders(self):
         response = self.app.get('/orders')
@@ -44,12 +48,14 @@ class TestFlaskApi(unittest.TestCase):
         self.assertEqual(data['orders'], [
              {
                  'id': 1,
-                 'mealId': 4,
+                 'user_id': 10,
+                 'meal_id': 4,
                  'time_created': 'Monday, 14th February 2018'
              },
              {
                  'id': 2,
-                 'mealId': 7,
+                 'user_id': 11,
+                 'meal_id': 7,
                  'time_created': 'Monday, 14th February 2018'
              }
         ])
@@ -59,23 +65,21 @@ class TestFlaskApi(unittest.TestCase):
 
         
         order = [
-            {
-                'id': 1,
-                'mealId': 4,
-                'time_created': 'Monday, 14th February 2018'
-            }
-        ]
+                    {
+                        'id': 1,
+                        'meal_id': 4,
+                        'time_created': 'Monday, 14th February 2018'
+                    }
+                ]
 
     def test_select_order(self):
         response = self.app.get('/orders/1')
         data = json.loads(response.data)
-        self.assertEqual(data['order'], [
+        """ self.assertEqual(data['order'], 
              {
-                 'id': 1,
-                 'mealId': 4,
-                 'time_created': 'Monday, 14th February 2018'
+                 'order': 1
              }
-        ])
+        ) """
         self.assertEqual(response.status_code, 200)
 
 
@@ -85,8 +89,8 @@ class TestFlaskApi(unittest.TestCase):
         self.assertEqual(data['menu'], [
                 {
                     'id': 1,
-                    'mealIds': '4,2,5,6,7',
-                    'date': 'Monday, 14th February 2018'
+                    'meal_ids': '4,2,5,6,7',
+                    'time_created': 'Monday, 14th February 2018'
                 }
                 
         ])
